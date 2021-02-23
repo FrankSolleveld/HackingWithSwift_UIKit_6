db6618b238f6d17b8f92e420c9fc5488f532af9c
FrankSolleveld <fsolleveld@outlook.com>
2021-02-23 09:08:00 +0100

    solved challenge 1

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -7,7 +7,6 @@
 
 /*
  CHALLENGE TIME
- 1. Try replacing the widthAnchor of our labels with leadingAnchor and trailingAnchor constraints.
  2. Once you've completed the first challenge, try using the safeAreaLayoutGuide for those constraints.
  3. Try making the height of your labels equal to 1/5th of the main view, minus 10 for the spacing.
  */
@@ -61,7 +60,8 @@ class ViewController: UIViewController {
         var previous: UILabel?
         
         for label in [label1, label2, label3, label4, label5] {
-            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
             label.heightAnchor.constraint(equalToConstant: 88).isActive = true
             if let previous = previous {
                 label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
